bcff51b33735717834f893cbc94d501c15d090d5
Nani1948 <nandhinikaluvakollu18@gmail.com>
2026-09-06 01:00:08 +0530

    Update both file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import "./App.css";
 
 function App(){
   return (
   <>
   {
-
+   /* Header is displayed on every page */
   }
   <Header/>
-
+   {/* Display the current route */}
   <main>
     <Outlet/>
   </main>
